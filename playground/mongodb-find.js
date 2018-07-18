eb1fbbc3f903024bc93a09425d1df8353f0a3a6a
daniel huizenga <dhuizenga@localhost.localdomain>
2018-07-18 10:13:01 +0200

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,28 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB');
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  //   // console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch:', err);
+  // })
+
+  db.collection('Users').find({
+    name: 'Daniel'
+  }).count().then((count) => {
+    console.log(`Todos count: ${count}`);
+    // console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch:', err);
+  })
+
+  db.close();
+});
